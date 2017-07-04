feec36de2cc821e8b81c9ca563ac42d6575e18c0
Ray Donnelly <mingw.android@gmail.com>
2017-07-04 07:51:47 +0100

    Add cross-compilation support (remove cyclic deps, add compiler)
    Removing the cyclic deps requires allowing 5 testsuite failures.
    See: http://lists.linuxfromscratch.org/pipermail/lfs-dev/2014-December/069805.html

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-./configure --prefix=$PREFIX
+./configure --prefix=${PREFIX}
 
 make
-make check
+if ! make check; then
+  FAILURES=$(cat tests/testsuite.log | grep FAILED | wc -l | awk '{print $1}')
+  if [[ ${FAILURES} > 5 ]]; then
+    echo "Expected 5 failures from the libtool testsuite due to cyclic dependency between libtool and autoconf/automake. Got ${FAILURES}"
+    echo "See: http://lists.linuxfromscratch.org/pipermail/lfs-dev/2014-December/069805.html"
+    echo "The expected failures are:"
+    echo "Standalone Libltdl."
+    echo "122: compiling softlinked libltdl"
+    echo "123: compiling copied libltdl"
+    echo "124: installable libltdl"
+    echo "125: linking libltdl without autotools"
+    echo "Subproject Libltdl."
+    echo "129: linking libltdl without autotools"
+    exit 1
+  fi
+fi
 make install
