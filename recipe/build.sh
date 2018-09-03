af391da1fa00daea91394a33730f1367899d8d6d
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-03 16:04:26 -0300

    remove .la

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-./configure --prefix=$PREFIX
+./configure --prefix=${PREFIX}
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
