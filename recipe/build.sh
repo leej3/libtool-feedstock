3846b1196955d9c5b76cb84254ec9170fc1ab5a3
Isuru Fernando <isuruf@gmail.com>
2020-08-28 10:10:52 -0500

    Don't store full paths

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,22 @@
 export HELP2MAN=$(which true)
 export M4=m4
 
+export AR=$(basename $AR)
+export AS=$(basename $AS)
+export STRIP=$(basename $STRIP)
+export RANLIB=$(basename $RANLIB)
+export LTCC=$(basename $CC)
+export LD=$(basename $LD)
+export CC=$(basename $CC)
+
+if [[ "$target_platform" == linux* ]]; then
+  export OBJDUMP=$(basename $OBJDUMP)
+else
+  export LIPO=$(basename $LIPO)
+  export NMEDIT=$(basename $NMEDIT)
+  export OTOOL=$(basename $OTOOL)
+fi
+
 ./configure --prefix=${PREFIX}
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
